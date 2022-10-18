13845186d49d688d269e2a41344139bc90c02276
Lucas <lucas_ramos95@outlook.com>
2022-10-17 23:52:39 -0300

    6th commit - código js refatorado

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
-const controle = document.querySelector(".controle-ajuste")
+const controle = document.querySelectorAll(".controle-ajuste")
 
-somar.addEventListener("click", () => {manipulaDados("somar")})
-
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", () => {
+        manipulaDados(evento.target.textContent)
+    })
+})
 
 function manipulaDados(operacao) {
-    if(operacao === "subtrair") {
+    if(operacao === "-") {
         braco.value = parseInt(braco.value) - 1
     } else {
         braco.value = parseInt(braco.value) + 1
